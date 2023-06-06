41e199aaf3cf3a656916c73e6f48da581ba71528
m3adel <mechatronics573@gmail.com>
2023-06-07 00:09:31 +0200

    Adding Task1

---
+++ Task1/DIO_driver_MISRA_compliant/DIO_driver_MISRA_compliant/LIB/std_types/std_types.h
@@ -0,0 +1,17 @@
+/*
+ * std_types.h
+ *
+ *  Created on: june 06, 2023
+ *      Author: Mohamed Abdelsalam - https://github.com/m3adel
+
+ */
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+typedef unsigned char u8;				/* 1 byte , 0 -> 255 */
+typedef signed char s8;					/* 1 byte , -128 -> 127 */
+typedef void vd;
+typedef _Bool boolean;
+#define  false 0
+#define  true 1
+#endif /* STD_TYPES_H_ */
---
+++ Task1/DIO_driver_MISRA_compliant/DIO_driver_MISRA_compliant/main.c
@@ -0,0 +1,18 @@
+/*
+ * DIO_driver_MISRA_compliant.c
+ *
+ * Created: 6/5/2023 6:24:00 PM
+ * Author : lenovo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
